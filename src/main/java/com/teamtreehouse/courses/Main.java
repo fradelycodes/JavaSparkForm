7d9dfb9b98abebdc502eb9b118a7338af0ac027a
fradelycodes <fradelydventura@gmail.com>
2023-10-30 06:47:18 -0400

    add java spark project

---
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -0,0 +1,32 @@
+package com.teamtreehouse.courses;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+//import static spark.Spark.put;
+
+
+public class Main {
+    public static void main(String[] args) {
+//        Welcome Page
+        get("/", (req, res) ->  {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+             return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+//        Sign in request
+        post("/sign-in", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            String username = req.queryParams("username");
+            res.cookie("username", username);
+            model.put("username", username);
+            return new ModelAndView(model,  "sign-in.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+}

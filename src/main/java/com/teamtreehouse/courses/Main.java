aa6e024f5089ebbb5d1de0843e2adba6aa4f639f
fradelycodes <fradelydventura@gmail.com>
2023-10-30 07:39:22 -0400

    add style scss/css

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -6,14 +6,15 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
+import static spark.Spark.*;
 //import static spark.Spark.put;
 
 
 public class Main {
     public static void main(String[] args) {
 //        Welcome Page
+        staticFiles.location("/public");
+
         get("/", (req, res) ->  {
             Map<String, String> model = new HashMap<>();
             model.put("username", req.cookie("username"));
